49389924aa8a2b1e74406035d9a19b0f6b10bf0e
takusan23 <takumu1200@gmail.com>
2019-11-08 13:00:06 +0900

    ナビゲーションドロワーにRecyclerViewつけた

--- app/src/main/java/io/github/takusan23/tatimidroid/NimadoActivity.kt
+++ app/src/main/java/io/github/takusan23/tatimidroid/NimadoActivity.kt
@@ -1,6 +1,8 @@
 package io.github.takusan23.tatimidroid
 
 import android.content.DialogInterface
+import android.content.res.Configuration
+import android.graphics.Color
 import android.graphics.Point
 import android.os.Bundle
 import android.view.View
@@ -8,11 +10,16 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.LinearLayout
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.button.MaterialButton
 import io.github.takusan23.tatimidroid.Fragment.CommentFragment
 import kotlinx.android.synthetic.main.activity_nimado.*
+import kotlinx.android.synthetic.main.fragment_gift_layout.*
 
 
 /*
@@ -20,9 +27,40 @@ import kotlinx.android.synthetic.main.activity_nimado.*
 * */
 class NimadoActivity : AppCompatActivity() {
 
+    lateinit var darkModeSupport: DarkModeSupport
+
+    var recyclerViewList: ArrayList<ArrayList<*>> = arrayListOf()
+    lateinit var nimadoListRecyclerViewAdapter: NimadoListRecyclerViewAdapter
+    lateinit var recyclerViewLayoutManager: RecyclerView.LayoutManager
+
+    //番組IDの配列
+    val programList = arrayListOf<String>()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //ダークモード
+        darkModeSupport = DarkModeSupport(this)
+
         setContentView(R.layout.activity_nimado)
+
+        //自作Toolbarを適用させる
+        setSupportActionBar(nimado_activity_toolbar)
+
+        //ステータスバーを透過する
+        window.statusBarColor = Color.TRANSPARENT
+
+        //ハンバーガーアイコンを実装
+        // sync drawer
+        val actionBarDrawerToggle = ActionBarDrawerToggle(
+            this, drawer_layout, nimado_activity_toolbar, R.string.nimado, R.string.nimado
+        )
+        drawer_layout.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+
+        //RecyclerView初期化
+        initRecyclerView()
 
         //追加ボタン
         nimado_activity_add_liveid_button.setOnClickListener {
@@ -35,6 +73,8 @@ class NimadoActivity : AppCompatActivity() {
                 .setPositiveButton("二窓開始") { dialogInterface: DialogInterface, i: Int ->
                     //番組ID
                     val liveId = editText.text.toString()
+
+                    programList.add(liveId)
 
                     //動的にView作成
                     val disp = windowManager.defaultDisplay
@@ -68,12 +108,28 @@ class NimadoActivity : AppCompatActivity() {
                     trans.replace(linearLayout.id, commentFragment, liveId)
                     trans.commit()
 
+                    //RecyclerViewついか
+                    val item = arrayListOf<String>()
+                    item.add("")
+                    item.add(liveId)
+                    item.add(liveId)
+                    recyclerViewList.add(item)
+                    nimadoListRecyclerViewAdapter.notifyDataSetChanged()
+
                 }
                 .show()
-
-
         }
+    }
 
+    private fun initRecyclerView() {
+        nimado_activity_list_recyclerview.setHasFixedSize(true)
+        val mLayoutManager = LinearLayoutManager(this)
+        nimado_activity_list_recyclerview.layoutManager = mLayoutManager as RecyclerView.LayoutManager?
+        nimadoListRecyclerViewAdapter = NimadoListRecyclerViewAdapter(recyclerViewList)
+        nimado_activity_list_recyclerview.adapter = nimadoListRecyclerViewAdapter
+        recyclerViewLayoutManager = gift_recyclerview.layoutManager!!
+        nimadoListRecyclerViewAdapter.linearLayout = nimado_activity_linearlayout
+        nimadoListRecyclerViewAdapter.activity = this
     }
 
     /*
---
+++ app/src/main/java/io/github/takusan23/tatimidroid/NimadoListRecyclerViewAdapter.kt
@@ -0,0 +1,77 @@
+package io.github.takusan23.tatimidroid
+
+import android.content.Context
+import android.content.Intent
+import android.database.sqlite.SQLiteDatabase
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
+import io.github.takusan23.tatimidroid.Fragment.CommunityListFragment
+import io.github.takusan23.tatimidroid.SQLiteHelper.AutoAdmissionSQLiteSQLite
+import kotlinx.android.synthetic.main.activity_nimado.*
+import org.w3c.dom.Text
+import java.util.*
+
+class NimadoListRecyclerViewAdapter(private val arrayListArrayAdapter: ArrayList<ArrayList<*>>) :
+    RecyclerView.Adapter<NimadoListRecyclerViewAdapter.ViewHolder>() {
+
+    var activity: NimadoActivity? = null
+    var linearLayout: LinearLayout? = null
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): NimadoListRecyclerViewAdapter.ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.adapter_auto_admission_layout, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return arrayListArrayAdapter.size
+    }
+
+    override fun onBindViewHolder(holder: NimadoListRecyclerViewAdapter.ViewHolder, position: Int) {
+
+        val item: ArrayList<String> = arrayListArrayAdapter[position] as ArrayList<String>
+
+        val title = item[1]
+        val id = item[2]
+
+        holder.programTitleTextView.text = title
+        holder.programIDTextView.text = id
+
+        //消すボタン
+        holder.closeButton.setOnClickListener {
+            //LinearLayoutと配列からけす
+            activity?.apply {
+                nimado_activity_linearlayout?.removeViewAt(position)
+                programList.removeAt(position)
+                //RecyclerViewからも
+                recyclerViewList.removeAt(position)
+                nimadoListRecyclerViewAdapter.notifyDataSetChanged()
+            }
+        }
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        var programTitleTextView: TextView
+        var programIDTextView: TextView
+        var closeButton: ImageButton
+
+        init {
+            programTitleTextView = itemView.findViewById(R.id.adapter_nimado_list_program_title)
+            programIDTextView = itemView.findViewById(R.id.adapter_nimado_list_program_id)
+            closeButton = itemView.findViewById(R.id.adapter_nimado_list_close_button)
+        }
+    }
+
+}
