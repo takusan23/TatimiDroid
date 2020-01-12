012ce3f92722d059131a42ccc6ab5d79f4bfaded
takusan23 <takumu1200@gmail.com>
2020-01-12 17:31:00 +0900

    6.2.0 りりーす？

--- app/src/main/java/io/github/takusan23/tatimidroid/Activity/KonoApp.kt
+++ app/src/main/java/io/github/takusan23/tatimidroid/Activity/KonoApp.kt
@@ -34,7 +34,7 @@ class KonoApp : AppCompatActivity() {
     /*
     * バージョンとか
     * */
-    val version = "2020/01/07　あけおめ！"
+    val version = "2020/01/12"
     val codeName1 = "（夏）"
 
     override fun onCreate(savedInstanceState: Bundle?) {
